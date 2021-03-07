a3d3669ee13f2695e8391d52a321cd462ad72780
SDen4 <asw325@yandex.ru>
2021-03-07 22:33:18 +0500

    title in Head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>The first post</title>
+      </Head>
+
       <h1>The first Post</h1>
 
       <div>
--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -1,17 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function SecondPost() {
   return (
     <>
+      <Head>
+        <title>The second post</title>
+      </Head>
+
       <h1>The second Post</h1>
 
       <h2>Image by Image component</h2>
 
       <Image
         src="/images/profile.jpg"
-        width={144}
-        height={144}
+        width={1440}
+        height={790}
         alt="Profile ava"
       />
 
